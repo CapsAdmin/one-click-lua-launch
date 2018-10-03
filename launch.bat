c98688dbd9167fd3e75276fdc02217cfc57c499b
CapsAdmin <eliashogstvedt@gmail.com>
2018-10-03 13:39:19 +0200

    better error handling

--- launch.bat
+++ launch.bat
@@ -32,7 +32,7 @@ SetLocal
 	
 	set cmd_line="!BINARY_DIR!\!BINARY_NAME!" "!SCRIPT_PATH!"
 	
-	IF RAN_FROM_FILEBROWSER equ 1 (
+	IF !RAN_FROM_FILEBROWSER! equ 1 (
 		start "" !cmd_line!
 	) else (
 		!cmd_line!
@@ -58,9 +58,8 @@ SetLocal
 		!abs_path! -e "os.exit(1)"
 
 		if !errorlevel! neq 1 (
-			echo "exit code from lua does not match 'os.exit(1)'"
+			call:AlertBox "exit code from lua does not match 'os.exit(1)'" "error"
 			del !abs_path!
-			echo "removing !abs_path!"
 
 			pause
 
@@ -77,50 +76,86 @@ goto:eof
 SetLocal
 	set url=%~1
 	set output_path=%~2
-	
-	if not exist !SystemRoot!\System32\where (
-	
+
+	if not exist !SystemRoot!\System32\where.exe (
 		set tmp_name=!TEMP!\lua_one_click_jscript_download.js
 		del /F !tmp_name! 2>NUL
-		echo //test >> !tmp_name!
+		echo //test > !tmp_name!
 		
 		if not exist !tmp_name! (
-			echo unable to create temp file !tmp_name! !
-			echo exiting
+			call:AlertBox "unable to create temp file !tmp_name! !" "error"
 			exit /b
 		)
 		
 		set forward_slash_path=!output_path:\=/!
 		
-		echo var req = new ActiveXObject^("Microsoft.XMLHTTP"^); >> !tmp_name!
-		echo req.Open^("GET","!url!",false^); >> !tmp_name!
-		echo req.Send^(^); >> !tmp_name!
-		echo var stream = new ActiveXObject^("ADODB.Stream"^); >> !tmp_name!
-		echo stream.Type = 1; >> !tmp_name!
-		echo stream.Open^(^);stream.Write^(req.responseBody^); >> !tmp_name!
+		echo try { >> !tmp_name!
+		echo var req = new ActiveXObject^("Microsoft.XMLHTTP"^) >> !tmp_name!
+		echo req.Open^("GET","!url!",false^) >> !tmp_name!
+		echo req.Send^(^) >> !tmp_name!
+
+		echo var stream = new ActiveXObject^("ADODB.Stream"^) >> !tmp_name!
+		echo stream.Type = 1 >> !tmp_name!
+		echo stream.Open^(^) >> !tmp_name!
+		echo stream.Write^(req.responseBody^) >> !tmp_name!
 		echo stream.SaveToFile^("!forward_slash_path!", 2^) >> !tmp_name!
-		echo stream.Close^(^); >> !tmp_name!
-		
-		cscript /E:JScript !tmp_name!
-		
+		echo stream.Close^(^) >> !tmp_name!
+		echo } catch^(err^) { >> !tmp_name!
+		echo 	WScript.Echo^("jscript error: "+err.message^) >> !tmp_name!
+		echo 	WScript.Quit^(1^) >> !tmp_name!
+		echo } >> !tmp_name!
+
+		cscript /Nologo /E:JScript !tmp_name!
+
+		if !errorlevel! neq 0 (
+			call:AlertBox "Failed to execute JScript to download file" "error"
+			goto:eof
+		)
+
 		del /F !tmp_name! 2>NUL
-		
+
 	) else (
 		where curl
 		if !errorlevel! equ 0 (
 			curl -L --url "!url!" --output "!output_path!"
+
+			if !errorlevel! neq 0 (
+				call:AlertBox "Curl failed to execute with error code !errorlevel!" "error"
+			)
+
 			goto:eof
 		) else (
 			where powershell
 			if !errorlevel! equ 0 (
-				PowerShell -NoLogo -NoProfile -NonInteractive "(New-Object System.Net.WebClient).DownloadFile('!url!','%~dp0!filename!')"
+				PowerShell -NoLogo -NoProfile -NonInteractive "(New-Object System.Net.WebClient).DownloadFile('!url!','!output_path!')"
+
+				if !errorlevel! neq 0 (
+					call:AlertBox "Powershell failed to execute with error code !errorlevel!" "error"
+				)
+
 				goto:eof
 			)
 		)
-		echo "unable to find curl or powershell"
+
+		call:AlertBox "unable to find curl or powershell"
 		exit /b
 	)
 EndLocal
+goto:eof
+
+:AlertBox
+SetLocal
+	set msg=%~1
+	set title=%~2
+
+	IF !RAN_FROM_FILEBROWSER! equ 1 (
+		mshta javascript:alert^("!msg!"^);close^(^);
+	) else (
+		echo !title!: !msg!
+	)
+
+EndLocal
+
 goto:eof
 rem ]]
 
